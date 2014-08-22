29afd159dbada209ae226ddd13b0029e0ab43bcd
王司徒 <wang.hao777@qq.com>
2014-08-22 10:28:43 +0800

    Java code eclipse git

---
+++ src/com/wst/test/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.wst.test;
+
+/**
+ * 第一个用git来管理的java项目
+ * 
+ * @blog  http://blog.csdn.net/wang_situ
+ * @author 王司徒
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git! -- Java");
+	}
+	
+}
